83ace38c7c014d6bdb700c9b8c3dc04b1af9bce2
manysin <s.many@dgc.gov.kh>
2024-08-12 08:36:42 +0700

    added slider

--- app/components/home/SlideComponent.tsx
+++ app/components/home/SlideComponent.tsx
@@ -3,7 +3,9 @@
 import { Carousel } from "flowbite-react";
 import Image from "next/image";
 import slide1 from "../../../public/slide1.png";
-import slide2 from "../../../public/slider2.png";
+import slide2 from "../../../public/1.png";
+import slide3 from "../../../public/2.png";
+import slide4 from "../../../public/3.png";
 
 export function SlideComponent() {
   return (
@@ -13,10 +15,13 @@ export function SlideComponent() {
           <Image width={1500} height={1500} src={slide1} alt="..." />
         </div>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          <Image width={1500} height={1500} src={slide1} alt="..." />
+          <Image width={1500} height={2000} src={slide2} alt="..." />
         </div>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          <Image width={1500} height={1500} src={slide1} alt="..." />
+          <Image width={1500} height={1000} src={slide3} alt="..." />
+        </div>
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          <Image width={1000} height={900} src={slide4} alt="..." />
         </div>
       </Carousel>
     </main>
