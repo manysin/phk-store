63463c7df4e43751362684db57a05801e75dfe91
manysin <s.many@dgc.gov.kh>
2024-09-25 17:15:49 +0700

    updated service

--- app/(user)/service/page.tsx
+++ app/(user)/service/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import { FaCircleDot } from "react-icons/fa6";
-import { RiArrowDropLeftLine, RiArrowDropRightLine } from "react-icons/ri";
+import service from "../../../public/service/service.png";
+import service1 from "../../../public/service/service1.png";
 
 export default function page() {
   return (
@@ -16,24 +17,37 @@ export default function page() {
             </h1>
           </div>
         </div>
-        <hr className="pb-16" />
+        <hr className="pb-20" />
         <section>
           <div className="flex justify-between">
             <Image
-              src=""
+              src={service}
               alt=""
-              width={100}
-              height={100} />
-            <p>To make sure our service is best for the customers, we have collabrated with the govenment to about all kinds of businesses.
-              Anyone would like to own their business on our web e-commerce must register with the govenment first. They have to make sure their store are legally.</p>
+              width={600}
+              height={600} />
+            <div className="ps-32 flex items-center">
+              <p className="font-semibold text-gray-500 text-xl leading-relaxed">
+                To make sure our service is best for the customers, we have collaborated with the government to regulate all kinds of
+                businesses. Anyone who would like to own their business on our web e-commerce must register with the government first.
+                They have to make sure their stores are legal.
+              </p>
+
+            </div>
           </div>
         </section>
       </section>
-      <section className="pt-16">
-        <h1 className="text-center font-bold text-2xl text-blue-900">
+      <section className="pt-24">
+        <h1 className="text-center font-bold text-4xl text-blue-900">
           Become Our Best Partner
         </h1>
-        <div className="flex flex-wrap gap-6 justify-center pt-12">
+        <div className="flex flex-wrap gap-6 justify-center py-20">
+          <div className=" bg-white shadow-sm h-60 max-w-96 w-72 rounded-md">
+            <Image
+              src={service1}
+              alt=""
+              width={500}
+              height={500} />
+          </div>
           <div className=" bg-white shadow-sm h-60 max-w-96 w-72 rounded-md">
             hi
           </div>
