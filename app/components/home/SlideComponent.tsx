3bf81961a4102c62fd06d8e77211089471a24777
manysin <s.many@dgc.gov.kh>
2024-09-26 14:54:02 +0700

    fix container

--- app/components/home/SlideComponent.tsx
+++ app/components/home/SlideComponent.tsx
@@ -9,8 +9,8 @@ import slide4 from "../../../public/./slide/3.png";
 
 export function SlideComponent() {
   return (
-    <main className="container">
-      <section className="h-56 sm:h-64 xl:h-80 2xl:h-96 mt-16">
+    <>
+      <section className="h-56 sm:h-64 xl:h-80 2xl:h-96 mt-16 container">
         <Carousel>
           <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
             <Image width={1500} height={1500} src={slide1} alt="..." />
@@ -26,6 +26,6 @@ export function SlideComponent() {
           </div>
         </Carousel>
       </section>
-    </main>
+    </>
   );
 }
